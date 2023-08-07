92edc9275b94cde5dabfae8e8af68a429b205e50
dncmumtaz <dncmumtaz@gmail.com>
2023-08-07 23:56:38 +0300

    refactor in entities, database conf

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public Context( IConfiguration configuration )
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+        {
+            // connect to postgres with connection string from app settings
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+    }
+}
