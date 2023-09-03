af83f0ebce9c910b0df1d3b915d9bd0dfe7c051e
dncmumtaz <dncmumtaz@gmail.com>
2023-09-03 21:42:52 +0300

    About page

---
+++ CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class AboutController : Controller
+	{
+		private Context context;
+		private AboutManager aboutManager;
+
+		public AboutController( Context context )
+		{
+			this.context = context;
+			aboutManager = new AboutManager(new EfAboutRepository(context));
+		}
+
+		public IActionResult Index()
+        {
+            var values = aboutManager.GetList();
+            return View(values);
+		}
+
+		public PartialViewResult SocialMediaAbout()
+		{
+			return PartialView();
+		}
+	}
+}
---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        private Context context;
+        private ContactManager contactManager;
+
+        public ContactController( Context context )
+        {
+            this.context = context;
+            contactManager = new ContactManager(new EfContactRepository(context));
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.Status = true;
+            contactManager.ContactAdd(p);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
