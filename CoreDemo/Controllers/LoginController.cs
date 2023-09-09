ea5315f648f9b9f5851676376bf21c96499e09fc
dncmumtaz <dncmumtaz@gmail.com>
2023-09-09 20:40:35 +0300

    Authentication and authorization process

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace CoreDemo.Controllers
 {
     public class LoginController : Controller
     {
+		Context context;
+
+		public LoginController( Context context )
+		{
+			this.context = context;
+		}
+
+		[AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
-    }
+
+
+		[AllowAnonymous]
+        [HttpPost]
+		public async Task<IActionResult> Index(Writer p)
+		{
+			var dataValue = context.Writers.FirstOrDefault(x => x.Email == p.Email
+			&& x.Password == p.Password);
+
+			if ( dataValue != null )
+			{
+				var claims = new List<Claim>
+				{
+					new Claim(ClaimTypes.Name, p.Email)
+				};
+				var userIdentity = new ClaimsIdentity(claims, "a");
+				ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+				await HttpContext.SignInAsync(principal);
+				return RedirectToAction("Index", "Writer");
+			}
+
+			return View(); 
+		}
+	}
 }
+/*
+ * var dataValue = context.Writers.FirstOrDefault(x => x.Email == p.Email
+			&& x.Password == p.Password);
+
+            if (dataValue != null)
+            {
+				HttpContext.Session.SetString("Name", p.Email);
+				return RedirectToAction("Index", "Writer");
+
+			}
+
+			return View();*/
--- CoreDemo/Program.cs
+++ CoreDemo/Program.cs
@@ -1,4 +1,7 @@
 using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +11,24 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<Context>( options
     => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsAssembly("DataAccessLayer")));
+
+builder.Services.AddSession();
+
+builder.Services.AddMvc(routes =>
+{
+	var policy = new AuthorizationPolicyBuilder().
+	RequireAuthenticatedUser().
+	Build();
+
+	routes.Filters.Add(new AuthorizeFilter(policy));
+});
+
+builder.Services.AddMvc();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x =>
+    {
+        x.LoginPath = "/Login/Index";
+    });
 
 var app = builder.Build();
 
@@ -21,6 +42,7 @@ if ( !app.Environment.IsDevelopment() )
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 #region for error codes
@@ -30,6 +52,7 @@ app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1", "?code ={0}");
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
