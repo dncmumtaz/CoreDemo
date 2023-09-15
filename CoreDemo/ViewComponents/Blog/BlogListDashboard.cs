3007914c61e0b59006fa4d329b777a72dcfa4067
dncmumtaz <dncmumtaz@gmail.com>
2023-09-15 23:27:12 +0300

    dashboard page

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        private Context context;
+        private BlogManager blogManager;
+
+        public BlogListDashboard( Context context )
+        {
+            this.context = context;
+            blogManager = new BlogManager(new EfBlogRepository(context));
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
