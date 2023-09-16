5ee9469271e907c585d11055cb7bca477fbc0c2c
dncmumtaz <dncmumtaz@gmail.com>
2023-09-16 17:05:18 +0300

    some fix in dashboard

---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        private Context context;
+        readonly CategoryManager categoryManager;
+
+        public CategoryListDashboard( Context context )
+        {
+            this.context = context;
+            categoryManager = new CategoryManager(new EfCategoryRepository(this.context));
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = categoryManager.GetList();
+            return View(values);
+        }
+    }
+}
