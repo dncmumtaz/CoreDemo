0bc6a55bab197476d1d2c2ae454b367c265f2930
dncmumtaz <dncmumtaz@gmail.com>
2023-09-16 19:06:17 +0300

    dasboard page: statistic feature

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        private Context context;
+
+        public DashboardController( Context context )
+        {
+            this.context = context;
+        }
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.v1 = context.Blogs.Count().ToString();
+            ViewBag.v2 = context.Blogs.Where(x => x.WriterId == 1).Count().ToString();
+            ViewBag.v3 = context.Categories.Count().ToString();
             return View();
         }
     }
