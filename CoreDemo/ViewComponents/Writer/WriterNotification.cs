4b3a901257ed1479f6a9c33f20dc88dcf793f837
dncmumtaz <dncmumtaz@gmail.com>
2023-09-17 20:22:04 +0300

    notif2

--- CoreDemo/ViewComponents/Writer/WriterNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterNotification.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterNotification : ViewComponent
     {
+        private Context context;
+        NotificationManager notificationManager;
+
+        public WriterNotification( Context context )
+        {
+            this.context = context;
+            notificationManager = new NotificationManager(new EfNotificationRepository(context));
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = notificationManager.GetList();
+            return View(values);
         }
     }
 }
