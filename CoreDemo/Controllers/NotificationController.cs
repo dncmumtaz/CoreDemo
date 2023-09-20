adcd1842b87f18faa79f9c4c43bb6577bcd8cc16
dncmumtaz <dncmumtaz@gmail.com>
2023-09-20 21:00:21 +0300

    notif list

--- CoreDemo/Controllers/NotificationController.cs
+++ CoreDemo/Controllers/NotificationController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class NotificationController : Controller
     {
+        private Context _dbContext;
+        private NotificationManager notificationManager;
+
+        public NotificationController( Context context )
+        {
+            _dbContext = context;
+            notificationManager = new NotificationManager(new EfNotificationRepository(context));
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult AllNotifications()
+        {
+            var values = notificationManager.GetList();
+            return View(values);
         }
     }
 }
