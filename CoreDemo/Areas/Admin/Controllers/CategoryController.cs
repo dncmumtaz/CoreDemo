53b9cd002632c7bbb14ae0c950e3e70e71e1565d
dncmumtaz <dncmumtaz@gmail.com>
2023-09-24 16:40:50 +0300

    listing categories

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private Context _dbContext;
+        CategoryManager categoryManager;
+
+        public CategoryController( Context dbContext )
+        {
+            _dbContext = dbContext;
+
+            categoryManager = new CategoryManager(new EfCategoryRepository(_dbContext));
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values = categoryManager.GetList();
+            return View(values);
         }
     }
 }
