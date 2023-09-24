c27b23f523a3f97ea82a32f715333ec2bf9c3909
dncmumtaz <dncmumtaz@gmail.com>
2023-09-24 19:19:54 +0300

    pagination

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
@@ -18,9 +19,9 @@ namespace CoreDemo.Areas.Admin.Controllers
             categoryManager = new CategoryManager(new EfCategoryRepository(_dbContext));
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var values = categoryManager.GetList();
+            var values = categoryManager.GetList().ToPagedList(page,2);
             return View(values);
         }
     }
--- CoreDemo/Program.cs
+++ CoreDemo/Program.cs
@@ -56,12 +56,14 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
